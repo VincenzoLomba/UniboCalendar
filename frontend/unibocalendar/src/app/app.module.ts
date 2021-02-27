2875f154a81b1390ea3658e7601d4b0b8a5d9285
VincenzoLomba <vincenzolombardi99@gmail.com>
2021-02-27 11:29:38 +0100

    Created Angular Router
    No route added yet

--- frontend/unibocalendar/src/app/app.module.ts
+++ frontend/unibocalendar/src/app/app.module.ts
@@ -3,12 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    // path: 'path', component: Component, canActive: [CanActive]
+  }
+];
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {
+      /* Reference: https://angular.io/api/router/ExtraOptions
+       *
+       * UseHash for Apache Tomcat:
+       * https://stackoverflow.com/questions/56363680/angular-routing-not-working-when-deployed-to-tomcat-9-ubuntu-18-04
+       * (notice that ApacheTomcat is not part of this project in the first place BUT it might will be in future releases)
+       */
+      useHash: true,
+      scrollPositionRestoration: 'enabled'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
